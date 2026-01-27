c29e53c4f64d9ebaad0e353dbbce14f8041c25ec
WilliamTRev <william.terry@revature.com>
2026-01-27 09:49:17 -0500

    static method created

--- Week1/PythonExamples/second_day_python.py
+++ Week1/PythonExamples/second_day_python.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 #from yesteryday with functions
 
 # you can even add functions as arguments in your functions
@@ -22,6 +24,46 @@ class MyNewClass:
     def my_new_class_function(self): # first parameter is always a reference to self, doesn't have to be called self
         return f"My name is {self.name} and I am {self.age} years old."
     #note we should use self instead of jeeves, but this is just to illustrate the point
+
+    # the __str__ method allows you to define what happens when you print an instance of the class
+    def __str__(self):
+        return f"MyNewClass(name={self.name}, age={self.age} from the to string method)"
     
+    # the __repr__ method allows you to create a clone of the class. I could create a new variables and call this method to create a clone of the object
+    def __repr__(self):
+        return f"MyNewClass(self, {self.age},{self.name})"
+
+# the self parameter is included because Python, under the hood, is actually using the class to call the function,
+# and the particular object you want to use is being passed  as the first parameter
+
 my_instance = MyNewClass(25, "Will")
 print(my_instance.my_new_class_function())
+
+my_class = MyNewClass()
+print(my_class.__str__())
+print(MyNewClass.__str__(my_class))  # equivalent to the above line
+print(my_class)  # implicitly calls __str__()
+
+#Python supports abstract classes. You make a class abstract by adding ABC inside the parentheses
+class MyAbstractClass(ABC):
+
+    #this is a class variable, accessed by calling the class itself, not an instantiated object
+    class_count: int = 0
+
+    #class methods take in the class as an implicity first argument,  can interact with and change the state of the class
+    @classmethod
+    def print_class_count(cls) -> int: 
+        return cls.class_count
+    #class methods take in the class as an implicity first argument,  can interact with and change the state of the class
+
+    #static methods don't take in an implicit first argument, they behave like regular functions but belong to the class's namespace
+    #static methods do not receive the class or an instance of the class as an implicit argument. You can add it,
+    #but you might as well use a class method at that point
+    @staticmethod
+    def static_method_example():
+        return "This is a static method example"
+
+
+print(MyAbstractClass.class_count)
+print(MyAbstractClass.print_class_count())
+MyAbstractClass.static_method_example()
