5c2e88093583960d667f2f5c62ec19ba4b4baac7
WilliamTRev <william.terry@revature.com>
2026-02-26 11:58:06 -0500

    added Spark day 1 code

--- Week6/Spark/SparkExample/SparkRDD1.py
+++ Week6/Spark/SparkExample/SparkRDD1.py
@@ -1,3 +1,5 @@
+from operator import add
+
 from pyspark.sql import SparkSession
 
 spark:SparkSession = SparkSession.builder \
@@ -6,8 +8,6 @@ spark:SparkSession = SparkSession.builder \
     .getOrCreate()
 
 sc = spark.sparkContext
-
-
 
 rdd1 = sc.parallelize(range(10))
 print(rdd1.collect())
@@ -70,7 +70,65 @@ k=b.join(d)
 
 print(k.toDebugString()) #to show lineage
 
+##reading from a file
+filename = "file:///home/will/pysparkFun2/pokemon.csv" 
+a = sc.textFile(filename)
+b= a.map(lambda line:line.split(" "))
 
+#map vs flatmap
+data = ["spark is fast",
+        "spark is powerful",
+        "spark is the best fast framework for data"]
+
+rdd=sc.parallelize(data)
+mapped = rdd.map(lambda line: line.split(" "))
+print(mapped.collect())
+
+flatmapped = rdd.flatMap(lambda line: line.split(" "))
+print(flatmapped.collect())
+
+#example wordcount
+words = rdd.flatMap(lambda line: line.split(" "))
+pairs = words.map(lambda word: (word,1))
+print(pairs.collect())
+
+word_counts = pairs.reduceByKey(lambda a,b:a+b)
+
+print(word_counts.collect())
+
+#could do the word count in one line 
+print("word count in one line from above")
+print(rdd.flatMap(lambda line: line.split(" ")).map(lambda word: (word,1)).reduceByKey(lambda a,b:a+b).collect())
+
+#Actions other than just collect
+a = sc.parallelize(range(1,10))
+a1=a.reduce(add)
+print(a.reduce(lambda a,b:a+b))
+
+#take and take ordered ar actions
+print(a1)
+print(a.takeOrdered(2))
+print(a.take(5))
+print(a.first())
+
+#countByKey and countByValue are actions
+c = sc.parallelize([(3,6),(5,8),(5,8),(3,"Dog")])
+print(c.countByKey())
+print(c.countByValue())
+
+## the following are more transformations not actions
+y = sc.parallelize([(3,6),(3,8),(5,8)])
+
+z=y.groupByKey().mapValues(list).collect()
+print(z)
+
+#flatMapValues similar to flatMap but for mapValues
+z=y.groupByKey().flatMapValues(list).collect()
+#TODO find a better example for flatMapValues
+
+print(y.keys().collect())
+print(y.values().collect())                                                                  
+print(y.sortByKey().collect())
 
 
 spark.stop()
